3945f5b6271871ad15aa1aa30cc58919388bd90d
alpha <alpha@AWMINI>
2024-05-24 12:34:17 +0200

    Projektdateien hinzufügen.

---
+++ InnoSetupCheckSumHelper/DateiInfo.vb
@@ -0,0 +1,13 @@
+﻿Option Strict On
+
+Public Class DateiInfo
+
+    Public DateiPfad As String
+    Public CheckSumme As String
+
+    Public Sub New(argDateiPfad As String, argCheckSumme As String)
+        DateiPfad = argDateiPfad
+        CheckSumme = argCheckSumme
+    End Sub
+
+End Class
